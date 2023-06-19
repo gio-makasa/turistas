d9bb843faa4f25949d13d73549376a2335e07602
gio <giorgimakasarashvili@gmail.com>
2023-06-19 15:21:02 +0400

    added header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <img src="../assets/logo.png" alt="logo" />
+    <nav>
+      <a>categories</a>
+      <a>services</a>
+      <a>tours</a>
+      <a>popular</a>
+    </nav>
+    <button>Log In</button>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+header {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 3rem;
+
+  img {
+    height: 100%;
+  }
+
+  nav {
+    display: flex;
+    height: 100%;
+
+    a {
+      display: flex;
+      align-items: center;
+      text-transform: uppercase;
+      border-left: 0.1rem solid black;
+      padding: 0 1.5rem;
+      transform: skewX(-10deg);
+      font-size: 1.1rem;
+
+      &:last-child {
+        border-right: 0.1rem solid black;
+      }
+
+      &:hover {
+        border-bottom: var(--main-green) 0.5rem solid;
+      }
+    }
+  }
+
+  button {
+    background-color: var(--main-green);
+    padding: 0.5rem 1.5rem;
+    margin-right: 1rem;
+    font-size: 1.1rem;
+    border: none;
+    border-radius: 0.5rem;
+  }
+}
+</style>
